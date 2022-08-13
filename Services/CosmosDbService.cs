692fbcfffabc2062f9e4d161ea2ed5b7d944d359
ckfg86 <vivek.gujari@motorolasolutions.com>
2022-08-13 16:53:07 -0600

    configure cosmosdb

--- Services/CosmosDbService.cs
+++ Services/CosmosDbService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,29 +9,49 @@ namespace SignUpForm_Backend.Services
 {
     public class CosmosDbService : ICosmosDbService
     {
-        public Task AddItemAsync(JObject data)
+        private Container _container;
+        public CosmosDbService(CosmosClient dbClient, string databaseName, string containerName)
         {
-            throw new NotImplementedException();
+            this._container = dbClient.GetContainer(databaseName, containerName);
+        }
+        public async Task AddItemAsync(JObject data)
+        {
+            await this._container.CreateItemAsync<JObject>(data);
         }
 
-        public Task DeleteItemAsync(string id)
+        public async Task DeleteItemAsync(string id)
         {
-            throw new NotImplementedException();
+            await this._container.DeleteItemAsync<JObject>(id, new PartitionKey(id));
         }
 
-        public Task<JObject> GetItemAsync(string id)
+        public async Task<JObject> GetItemAsync(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ItemResponse<JObject> response = await this._container.ReadItemAsync<JObject>(id, new PartitionKey(id));
+                return response;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
         }
 
-        public Task<IEnumerable<JObject>> GetItemsAsync(string query)
+        public async Task<IEnumerable<JObject>> GetItemsAsync(string queryString)
         {
-            throw new NotImplementedException();
+            var query = this._container.GetItemQueryIterator<JObject>(new QueryDefinition(queryString));
+            List<JObject> results = new List<JObject>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+            return results;
         }
 
-        public Task UpdateItemAsync(string id, JObject data)
+        public async Task UpdateItemAsync(string id, JObject data)
         {
-            throw new NotImplementedException();
+            await this._container.UpsertItemAsync<JObject>(data, new PartitionKey(id));
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SignUpForm_Backend.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace SignUpForm_Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<ICosmosDbService>
+                (InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +50,20 @@ namespace SignUpForm_Backend
             {
                 endpoints.MapControllers();
             });
+        }
+
+        // create a cosmos db database and a container with the specified partition key
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            CosmosClient client = new CosmosClient(account, key);
+            CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+            return cosmosDbService;
         }
     }
 }
